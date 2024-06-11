5f7c5a406d1f0467d2bb7e43515a268d2d431753
Steve Fox <stevefox.dev@gmail.com>
2024-06-11 09:26:17 -0400

    add error flash message

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -40,12 +40,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 // Show
 router.get('/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'Cannot Find Campground');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', {campground});
 }));
 
 // Edit
 router.get('/:id/edit', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'Cannot Find Campground');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', {campground});
 }));
 
