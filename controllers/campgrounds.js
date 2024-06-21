8a48f76c91e1e6ea5dfd8e42c85b04ba76d52a3a
Steve Fox <stevefox.dev@gmail.com>
2024-06-21 13:31:56 -0400

    install mapbox geocoding packages, take location data and geocode it, save the geocode data in db

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 // index campgrounds
 module.exports.index = async (req, res, next) => {
@@ -14,10 +17,16 @@ module.exports.renderNewForm = (req, res, next) => {
 
 // create campground
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send();
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
+    console.log(campground);
     req.flash('success', 'Successfully Made A New Campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }
