59527ab2e502ead45bf32f40f7f7c902cb1a7b62
Steve Fox <stevefox.dev@gmail.com>
2024-06-25 15:34:38 -0400

    fix JSON stringify to JSON parse bug to push campground object to mapbox file, add popup with title and location to map pin

--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -3,11 +3,18 @@ mapboxgl.accessToken = mapBoxToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: campCoordinates, // starting position [lng, lat]
+    style: 'mapbox://styles/mapbox/satellite-v9', 
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 13, // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat(campCoordinates)
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h5>${campground.title}</h5
+            <p>${campground.location}</p>`
+        )
+    )
     .addTo(map);
